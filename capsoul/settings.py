bc5e9284f63d7fb9330771a16e2fa1f4b7466dcb
Eric Marcondes <marcer@csl04.cs.wallawalla.edu>
2017-11-09 16:33:04 -0800

    working on media root and url

--- capsoul/settings.py
+++ capsoul/settings.py
@@ -144,3 +144,6 @@ STATIC_URL = '/static/'
 
 # CORS Headers fix
 CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- database/models.py
+++ database/models.py
@@ -49,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     date_of_birth = models.DateField(null=True, blank=True)
-    photo = models.FileField()
+    photo = models.ImageField(blank=True)
     email = models.EmailField()
     phone = models.IntegerField(null=True, blank=True)
     location = models.TextField()
@@ -82,11 +82,13 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Capsule(models.Model):
     cid = models.AutoField(primary_key = True)
     unlocks_at = models.DateTimeField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='+')
     contributors = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='+')
     recipients = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='+')
     title = models.TextField()
     description = models.TextField()
+    letter = models.FileField(blank=True)
+    media = models.FileField(blank=True)
 
     def __str__(self):
         return self.title
