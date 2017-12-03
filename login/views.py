726883f107720ef0d887dcb1819162b4d2fcb3f6
Jonathan De Leon <jonathan.deleon@wallawalla.edu>
2017-12-03 13:36:18 -0800

    Added support for error checking. Updated HTTP request status codes

--- login/views.py
+++ login/views.py
@@ -4,17 +4,16 @@ from __future__ import unicode_literals
 import json
 
 from django.contrib.auth.models import AnonymousUser
-from django.http import Http404
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from django.core import serializers
 
+from rest_framework import status
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from database.models import ExpiringToken
 
@@ -24,56 +23,60 @@ UserModel = get_user_model()
 # Create your views here.
 @api_view(['POST'])
 @permission_classes((AllowAny, ))
-def ajax_login(request, *args, **kwargs):
-    data = json.loads(request.body)
-    username = data['username']
-    password = data['password']
+def ajax_login(request):
+    try:
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+    except:
+        return Response({'status':'Params not set'}, status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(request, username=username, password=password)
     if user is None:
-        return Response({'status':'No user matches the given query'}, status=404)
-    login(request, user)
+        return Response({'status':'No user matches the given query'}, status=status.HTTP_404_NOT_FOUND)
     token, created = ExpiringToken.objects.get_or_create(user=user)
-    response = Response({'status':'login successful','token':token.key, 'user':json.loads(serializers.serialize('json', [user, ]))}, status=200)
-    response.set_cookie('session', token.key)
+    response = Response({'status':'login successful','token':token.key, 'user':json.loads(serializers.serialize('json', [user, ]))}, status=status.HTTP_200_OK)
+    # response.set_cookie('token_session', token.key)
     return response
 
 @api_view(['POST'])
 @permission_classes((AllowAny, ))
 def register(request):
-    data = json.loads(request.body)
-    username = data['username']
-    password = data['password']
-    user = authenticate(request, username=username, password=password, is_active=True)
+    try:
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+    except:
+        return Response({'status':'Params not set'}, status=status.HTTP_400_BAD_REQUEST)
+    user = UserModel.objects.get(username=username)
     if user is not None:
-        return Response({'status':'User already exists'}, status=404)
+        return Response({'status':'User already exists'}, status=status.HTTP_404_NOT_FOUND)
     user = UserModel.objects.create_user(username, password)
     token, created = ExpiringToken.objects.get_or_create(user=user)
-    response = Response({'status':'User has been successfully created','token':token.key}, status=200)
-    response.set_cookie('token_session', token.key)
+    response = Response({'status':'User has successfully been created','token':token.key}, status=status.HTTP_201_CREATED)
+    # response.set_cookie('token_session', token.key)
     return response
 
 @api_view(['GET', 'POST'])
 def ajax_logout(request):
-    logout(request)
-    response = Response({'status':'Successfully logged out'}, status=200)
+    response = Response({'status':'Successfully logged out'}, status=status.HTTP_200_OK)
     response.delete_cookie('token_session')
     return response
 
 @api_view(['GET'])
 def verify(request):
-    # cookie = request.COOKIES.get('token_session')
-    # if cookie is None:
     if request.user is AnonymousUser:
-        return Response({'status':'Invalid token'}, status=404)
-    # token, _ = ExpiringToken.objects.get_or_create(token=cookie)
-    # user = ExpiringToken.objects.get(key=cookie).user
-    return Response({'status':'token is verified','user':json.loads(serializers.serialize('json', [request.user, ]))}, status=200)
+        return Response({'status':'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED, headers='WWW-Authenticate: Token')
+    return Response({'status':'token is verified','user':json.loads(serializers.serialize('json', [request.user, ]))}, status=status.HTTP_200_OK)
 
 class ObtainExpiringAuthToken(ObtainAuthToken):
     """View enabling username/password exchange for expiring token"""
     model = ExpiringToken
     def post(self, request):
-        serializer = AuthTokenSerializer(data=request.data)
+        try:
+            data = json.loads(request.body)
+        except:
+            return Response({'status':'Params not set'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = AuthTokenSerializer(data=data)
         if serializer.is_valid():
             token, _ = ExpiringToken.objects.get_or_create(
                 user=serializer.validated_data['user']
@@ -85,7 +88,7 @@ class ObtainExpiringAuthToken(ObtainAuthToken):
                 )
             data = {'token': token.key}
             return Response(data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 obtain_expiring_auth_token = ObtainExpiringAuthToken.as_view()
 
