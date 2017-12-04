3b7fe5e6c53ee9d785671266cf529da83035e257
Ethan Beaver <ethan.beaver@ahss.org>
2017-12-04 09:48:15 -0800

    Creation of profile photos

--- users/views.py
+++ users/views.py
@@ -12,22 +12,29 @@ from rest_framework.response import Response
 from database.models import User
 
 
-@api_view(['GET', 'POST'])
+# @api_view(['GET', 'POST'])
 def all_users(request):
     if request.method == 'GET':
         all_users = User.objects.all().values('username', 'first_name', 'last_name')
         return JsonResponse({'users': list(all_users)}, status=200)
     else:
-        fields = json.loads(request.body)
+        fields = {}
+        try:
+            fields = json.loads(request.body)
+        except ValueError:
+            for field in request.POST:
+                fields[field] = request.POST[field]
         fields['username'] = request.user.username
         current_user = User(**fields)
         current_user.save()
-        return Response({"status": "resource created"}, status=200)
+        current_user.photo = request.FILES['photo']
+        current_user.save()
+        return JsonResponse({"status": "profile updated"}, status=200)
 
 
 @api_view(['GET'])
 def specific_user(request, uname):
     user = User.objects.filter(username=uname).values()
     if not user:
-        return Response({"status": "No user matches the given query."}, status=404)
+        return JsonResponse({"status": "No user matches the given query."}, status=404)
     return JsonResponse(list(user)[0], status=200)
